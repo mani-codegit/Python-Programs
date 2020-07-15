8d59507b71103b41bd93c4c391d26de474a465cc
Mani Bharathi <51756531+mani-codegit@users.noreply.github.com>
2020-07-15 09:46:19 +0530

    Add files via upload

---
+++ 1. Welcome.py
@@ -0,0 +1,5 @@
+#Get the user name as the input and welcome the user
+user_name=input("Enter the name ")
+start = "\033[1m "
+end = "\033[0;0m"
+print("Hello"+start+ f"{user_name}"+end+" welcome to my python programs")
