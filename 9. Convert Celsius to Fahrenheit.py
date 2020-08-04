7953ba51463bf50e5b52e58134b83b1dd92e1a33
Mani Bharathi <51756531+mani-codegit@users.noreply.github.com>
2020-08-04 17:49:01 +0530

    Add files via upload

---
+++ 9. Convert Celsius to Fahrenheit.py
@@ -0,0 +1,14 @@
+#Convert to celsius to fahrenheit
+Celsius=float(input("Enter the Celsius "))
+out=(Celsius*9/5)+32
+
+"""
+#Convert fahrenheit to celsius
+Fahrenheit=float(input("Enter the Fahrenheit "))
+out=(Fahrenheit-32)*5/9
+print(f"{Fahrenheit} fahrenheit in celsius is {out}")
+
+"""
+
+print(f" {Celsius} celsius in fahrenheit is {out}")
+
