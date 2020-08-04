6d5dbcaa32a0a4eb3df6ac61bb355be95fb9dce1
Mani Bharathi <51756531+mani-codegit@users.noreply.github.com>
2020-08-04 20:43:52 +0530

    Add files via upload

---
+++ 10. Check the Number Positive or Negative or Zero.py
@@ -0,0 +1,9 @@
+#Check the Number Positive or Negative or Zero
+Num=float(input("Enter the Number: "))
+
+if (Num>0):
+        print("Number is Positive Number")
+elif Num==0:
+    print("Number is Zero")
+else:
+    print("Number is Negative Number")
