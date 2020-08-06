5ca8a6194c12b8a8ce3e0c2c5a4d199ecc02bbd5
Mani Bharathi <51756531+mani-codegit@users.noreply.github.com>
2020-08-06 14:16:38 +0530

    Add files via upload

---
+++ 17. print the ceiling floor and round of the value of given floating number.py
@@ -0,0 +1,10 @@
+#print the ceiling floor and round of the value of given floating number
+import math
+num=float(input("Enter the number floating number"))
+ceil=math.ceil(num)
+floor=math.floor(num)
+roundoff=round(num)
+
+print(f"Ceil the value is {ceil}")
+print(f"Floor the value is {floor}")
+print(f"Round the value is {roundoff}")
