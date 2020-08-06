d2916488ccaf9e442888e8ba0ca9726151648245
Mani Bharathi <51756531+mani-codegit@users.noreply.github.com>
2020-08-06 14:02:18 +0530

    Add files via upload

---
+++ 16. Read two numbers and print there quotient and remainders.py
@@ -0,0 +1,8 @@
+#Read two numbers and print there quotient and remainders
+
+num1=float(input("Enter the  First Number: "))
+num2=float(input("Enter the  Second Number: "))
+quotient=num1/num2
+remainder=num1%num2
+print(f"The Quetient is {quotient}")
+print(f"The Remainder is {remainder}")
